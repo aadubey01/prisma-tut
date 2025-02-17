0ce7e5d7064bdb9570829d20f66931889d91a288
Aakriti Dubey <aakritidubey51@gmail.com>
2025-02-18 00:23:50 +0530

    Update schema amd index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,55 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma  = new PrismaClient();
-async function insertUser(username: string, password: string, firstname: string, lastname: string) {
-  const res = await prisma.users.create({
-    data:{
-       username,
-       password,
-       firstname ,
-       lastname 
+// async function insertUser(username: string, password: string, firstname: string, lastname: string) {
+//   try{const res = await prisma.users.create({
+//     data:{
+//        username,
+//        password,
+//        firstname ,
+//        lastname 
+//     }
+//   });
+//   console.log(res)}
+//   catch(error){
+//     console.error("Error inserting user",error);
+//   }
+// }
+
+// interface UpdateParams{
+//   firstname:string
+//   lastname :string
+// }
+
+// async function UpdateUser(username:string,{
+//   firstname,
+//   lastname
+// }:UpdateParams){
+//  const res = await prisma.users.update({
+//     where:{username:username},
+//     data:{
+//       firstname,
+//       lastname
+//     }
+//   })
+//   console.log(res);
+
+// }
+
+async function getUser(username:string){
+  const res = await prisma.users.findFirst({
+    where:{
+      username:username
     }
   })
   console.log(res)
+
 }
 
-insertUser("Hames%7","password","JAmes","Hanks")
+// insertUser("Hames%7","password","JAmes","Hanks")
+// UpdateUser("Hames%7",{
+//   firstname:"Aakriti",
+//   lastname:"Dubey"
+// })
+
+getUser("Hames%7");
